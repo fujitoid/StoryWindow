90cf04bda060f273943e86e727803d03df9b9af1
fujitoid <provisorc@gmail.com>
2022-05-18 19:31:43 +0200

    Drag n drop

--- StoryWindow/Assets/Scripts/View/Runtime/NodeView.cs
+++ StoryWindow/Assets/Scripts/View/Runtime/NodeView.cs
@@ -16,15 +16,19 @@ namespace Nekonata.SituationCreator.StoryWindow.View.Runtime
         private BaseNode _node;
         private Action<BaseNode> _onNodeSelected;
 
+        private VisualElement _dradAndDropHolder;
+
+        private bool _isMouseHolded = false;
+
         public BaseNode Node => _node;
 
         public RadioButton Input => _leftInput;
         public RadioButton Output => _rightOutput;
 
-        public NodeView(BaseNode baseNode)
-        {
-            this.AddManipulator(new Clickable(OnClick, 0, 0));
+        public new class UxmlFactory : UxmlFactory<NodeView, NodeView.UxmlTraits> { }
 
+        public NodeView()
+        {
             this.style.position = Position.Absolute;
             this.style.width = 135;
             this.style.height = 75;
@@ -33,24 +37,58 @@ namespace Nekonata.SituationCreator.StoryWindow.View.Runtime
             _textField = new TextField(string.Empty);
             _textField.value = "NodeName";
             _textField.name = "text-field";
-            _textField.AddManipulator(new Clickable(OnClick, 0, 0));
             this.Add(_textField);
 
             var visualElement = new VisualElement();
             visualElement.style.flexDirection = FlexDirection.Row;
-            visualElement.AddManipulator(new Clickable(OnClick, 0, 0));
             this.Add(visualElement);
 
             _leftInput = new RadioButton(string.Empty);
             _leftInput.style.width = 95;
             _leftInput.name = "left-input";
-            _leftInput.AddManipulator(new Clickable(OnClick, 0, 0));
             visualElement.Add(_leftInput);
 
             _rightOutput = new RadioButton(string.Empty);
             _rightOutput.style.width = 25;
             _rightOutput.name = "right-output";
-            _rightOutput.AddManipulator(new Clickable(OnClick, 0, 0));
+            visualElement.Add(_rightOutput);
+
+            _dradAndDropHolder = new VisualElement();
+            _dradAndDropHolder.style.position = Position.Absolute;
+            _dradAndDropHolder.style.left = 36;
+            _dradAndDropHolder.style.top = 45;
+            _dradAndDropHolder.style.width = 65;
+            _dradAndDropHolder.style.height = 30;
+            _dradAndDropHolder.RegisterCallback<MouseDownEvent>(OnMouseDown);
+            _dradAndDropHolder.RegisterCallback<MouseUpEvent>(OnMouseUp);
+            _dradAndDropHolder.RegisterCallback<MouseMoveEvent>(OnMouseMove);
+            this.Add(_dradAndDropHolder);
+        }
+
+        public NodeView(BaseNode baseNode)
+        {
+            this.style.position = Position.Absolute;
+            this.style.width = 135;
+            this.style.height = 75;
+            this.style.backgroundColor = new Color(0.3411765f, 0.3411765f, 0.3411765f, 1);
+
+            _textField = new TextField(string.Empty);
+            _textField.value = "NodeName";
+            _textField.name = "text-field";
+            this.Add(_textField);
+
+            var visualElement = new VisualElement();
+            visualElement.style.flexDirection = FlexDirection.Row;
+            this.Add(visualElement);
+
+            _leftInput = new RadioButton(string.Empty);
+            _leftInput.style.width = 95;
+            _leftInput.name = "left-input";
+            visualElement.Add(_leftInput);
+
+            _rightOutput = new RadioButton(string.Empty);
+            _rightOutput.style.width = 25;
+            _rightOutput.name = "right-output";
             visualElement.Add(_rightOutput);
 
             this._node = baseNode;
@@ -101,10 +139,39 @@ namespace Nekonata.SituationCreator.StoryWindow.View.Runtime
             Vector2 positionToSave = new Vector2(newPosition.xMin, newPosition.yMin);
             _node.SetPosition(positionToSave);
         }
-
-        private void OnClick()
+        
+        public void SetPostion(Vector2 newPosition)
         {
-            Debug.Log(_node.Name);
+            this.style.top = newPosition.y - this.layout.height / 2;
+            this.style.left = newPosition.x - this.layout.width / 2;
+        }
+
+        private void OnMouseDown(MouseDownEvent downEvent)
+        {
+            _isMouseHolded = true;
+
+            _dradAndDropHolder.style.left = 0;
+            _dradAndDropHolder.style.top = 0;
+            _dradAndDropHolder.style.width = 135;
+            _dradAndDropHolder.style.height = 75;
+        }
+
+        private void OnMouseUp(MouseUpEvent mouseUp)
+        {
+            _isMouseHolded = false;
+
+            _dradAndDropHolder.style.left = 36;
+            _dradAndDropHolder.style.top = 45;
+            _dradAndDropHolder.style.width = 65;
+            _dradAndDropHolder.style.height = 30;
+        }
+
+        private void OnMouseMove(MouseMoveEvent moveEvent)
+        {
+            if (_isMouseHolded == false)
+                return;
+
+            SetPostion(moveEvent.mousePosition);
         }
     }
 }
